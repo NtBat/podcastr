2c71a2d7acba59ee35a21aa1ee7cead375f40e30
Nathan Batista <nathan.batista@codenome.tech>
2021-07-02 15:42:58 -0300

    push github

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import light from '../styles/themes/light';
+import dark from '../styles/themes/dark';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+
+import GlobalStyle from '../styles/global';
+import { AppComponent } from '../styles/app';
+import { PLayerContextProvider } from '../contexts/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
+  return (
+    <PLayerContextProvider>
+      <ThemeProvider theme={theme}>
+        <AppComponent>
+          <GlobalStyle />
+          <main>
+            <Header toggleTheme={toggleTheme}/>
+            <Component {...pageProps} />
+          </main>
+          <Player />
+        </AppComponent>
+      </ThemeProvider>
+    </PLayerContextProvider>
+  );
+}
+
+export default MyApp
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,27 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary300: '#9f75ff',
+    primary400: '#9164fa',
+    primary500: '#650dcd',
+    primary800: '#6f48c9',
+
+    secondary: '#04d361',
+
+    backgroundDeep: '#1C2B33',
+    backgroundPrimary: '#28293e',
+    backgroundSecondary: '#1c1c28',
+
+    text50: '#494d4b',
+    text100: '#808080',
+    text200: '#afb2b1',
+    text500: '#e6e8eb',
+    text800: '#f7f8fa',
+
+    base: '#000',
+
+    black: '#000',
+    white: '#fff',
+  }
+}
